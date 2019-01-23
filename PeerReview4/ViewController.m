037b648f4fa89b66c54f1666887d2a4085bf1b1e
Ananta Shahane <ananta@Anantas-MacBook-Air.local>
2019-01-23 20:30:41 +0530

    How Far?

--- PeerReview4/ViewController.m
+++ PeerReview4/ViewController.m
@@ -7,12 +7,129 @@
 //
 
 #import "ViewController.h"
+#import "DistanceGetter/DGDistanceRequest.h"
 
 @interface ViewController ()
+
+@property (nonatomic) DGDistanceRequest *req;
+@property (weak, nonatomic) IBOutlet UITextField *SourceLocation;
+@property (weak, nonatomic) IBOutlet UITextField *DestinationA;
+@property (weak, nonatomic) IBOutlet UITextField *DestinationB;
+@property (weak, nonatomic) IBOutlet UITextField *DestinationC;
+@property (weak, nonatomic) IBOutlet UILabel *DistanceA;
+@property (weak, nonatomic) IBOutlet UILabel *DistanceB;
+@property (weak, nonatomic) IBOutlet UILabel *DistanceC;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *UnitController;
+
+@property (weak, nonatomic) IBOutlet UIButton *CalculateButton;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)CalculateButton:(id)sender {
+    self.CalculateButton.enabled = NO;
+    NSString *source = self.SourceLocation.text;
+    NSString *destinationA = self.DestinationA.text;
+    NSString *destinationB = self.DestinationB.text;
+    NSString *destinationC = self.DestinationC.text;
+    NSArray *dests = @[destinationA, destinationB, destinationC];
+    self.req = [DGDistanceRequest alloc];
+    
+    self.req = [self.req initWithLocationDescriptions:dests sourceDescription:source];
+    
+    __weak ViewController *weakSelf = self;
+    
+    self.req.callback = ^void(NSArray *responses){
+        ViewController *strongSelf = weakSelf;
+        if (!strongSelf) {
+            return;
+        }
+        NSNull *badResult = [NSNull null];
+        
+        if (responses[0] != badResult) {
+            double number = ([responses[0] floatValue]);
+            if (strongSelf.UnitController.selectedSegmentIndex == 0) {
+                NSString *x = [NSString stringWithFormat:@"%.2f m", number];
+                strongSelf.DistanceA.text = x;
+            }
+            else if(strongSelf.UnitController.selectedSegmentIndex == 1)
+            {
+                double kms = number/1000;
+                NSString *x = [NSString stringWithFormat:@"%.2lf km", kms];
+                weakSelf.DistanceA.text = x;
+            }
+            else
+            {
+                double miles = number/1609.34;
+                NSString *x = [NSString stringWithFormat:@"%.2lf miles", miles];
+                weakSelf.DistanceA.text = x;
+            }
+            
+        }
+        else
+        {
+            NSString *x = @"Unknown Distance";
+            weakSelf.DistanceA.text = x;
+            
+        }
+        if (responses[1] != badResult) {
+            double number = ([responses[1] floatValue]);
+            if (strongSelf.UnitController.selectedSegmentIndex == 0) {
+                NSString *x = [NSString stringWithFormat:@"%.2f m", number];
+                strongSelf.DistanceB.text = x;
+            }
+            else if(strongSelf.UnitController.selectedSegmentIndex == 1)
+            {
+                double kms = number/1000;
+                NSString *x = [NSString stringWithFormat:@"%.2lf km", kms];
+                weakSelf.DistanceB.text = x;
+            }
+            else
+            {
+                double miles = number/1609.34;
+                NSString *x = [NSString stringWithFormat:@"%.2lf miles", miles];
+                weakSelf.DistanceB.text = x;
+            }
+        }
+        else
+        {
+            NSString *x = @"Unknown Distance";
+            weakSelf.DistanceB.text = x;
+            
+        }
+        if (responses[2] != badResult) {
+            double number = ([responses[2] floatValue]);
+            if (strongSelf.UnitController.selectedSegmentIndex == 0) {
+                NSString *x = [NSString stringWithFormat:@"%.2f m", number];
+                strongSelf.DistanceC.text = x;
+            }
+            else if(strongSelf.UnitController.selectedSegmentIndex == 1)
+            {
+                double kms = number/1000;
+                NSString *x = [NSString stringWithFormat:@"%.2lf km", kms];
+                weakSelf.DistanceC.text = x;
+            }
+            else
+            {
+                double miles = number/1609.34;
+                NSString *x = [NSString stringWithFormat:@"%.2lf miles", miles];
+                weakSelf.DistanceC.text = x;
+            }
+        }
+        else
+        {
+            NSString *x = @"Unknown Distance";
+            weakSelf.DistanceC.text = x;
+            
+        }
+        strongSelf.CalculateButton.enabled = YES;
+        strongSelf.req = nil;
+        
+    };
+    [self.req start];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
---
+++ README.md
@@ -0,0 +1,14 @@
+# Peer Review 4
+#### A prototype project for finding the displacement between a source and 3 destinations.
+##### This project is 4/15 Peer Review projects done in the course \"iOS Development for Creative Entrepreneurs\" by UCI.
+___
+In this project we use a cocoa-pod ```DistanceGetter 0.1```.
+This project has 4 *UITextField*s. One of them is used to get the Source Location from the user and other three are Destination locations.
+
+It has a *UISegmentControl* for selecting measurement units.
+
+And it has a UIButton to trigger the request.
+___
+
+![PeerReview4](https://github.com/Ananta11/PeerReview4/raw/master/Common/Screenshot.png)
+___
